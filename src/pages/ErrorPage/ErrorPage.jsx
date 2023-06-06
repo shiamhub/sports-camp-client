71c418f161586dc5c23c436242a4814eb00d86e2
shiamhub <skshiam@gmail.com>
2023-06-07 01:14:31 +0600

    first commit

---
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,10 @@
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h1>404</h1>
+        </div>
+    );
+};
+
+export default ErrorPage;
---
+++ src/routes/Routes.jsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layout/Main";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
+import Home from "../pages/Home/Home";
+
+const Routes = createBrowserRouter([
+    {
+        path: "/",
+        element: <Main></Main>,
+        errorElement: <ErrorPage></ErrorPage>,
+        children: [
+            {
+                path: "/",
+                element: <Home></Home>,
+            }
+        ]
+    }
+])
+
+export default Routes;
